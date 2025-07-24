de6487e203ca906b8652c4308f148c10639307b3
andrew@asc-solutions.co.uk <andrew@asc-solutions.co.uk>
2025-07-24 20:02:33 +0100

    moved ollama deployment to terraform

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,64 @@
+
+
+resource "kubernetes_deployment" "ollama" {
+  metadata {
+    name = "ollama"
+    labels = {
+      app = "ollama"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "ollama"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "ollama"
+        }
+      }
+
+      spec {
+        container {
+          name  = "ollama"
+          image = "ollama/ollama:latest"
+
+          port {
+            container_port = 11434
+          }
+
+          env {
+            name  = "OLLAMA_PORT"
+            value = "8080"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "ollama_service" {
+  metadata {
+    name = "ollama-service"
+  }
+
+  spec {
+    selector = {
+      app = "ollama"
+    }
+
+    port {
+      port        = 8080
+      target_port = 11434
+      protocol    = "TCP"
+    }
+
+    type = "LoadBalancer"
+  }
+}
